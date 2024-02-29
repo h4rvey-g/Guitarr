293adad0efa81bf1a315358d0a0264e498ca50eb
babaolanqiu <babaolanqiu@gmail.com>
2024-02-29 22:06:53 +0800

    Add alignment script for genome and SIRV references

---
+++ codes/03.align.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+index_dir="./data/reference/STARindex"
+mkdir -p $index_dir
+if [ -z "$(ls -A ${index_dir})" ]; then
+    STAR \
+        --runMode genomeGenerate \
+        --genomeDir ${index_dir} \
+        --runThreadN 20 \
+        --genomeFastaFiles data/reference/GRCh38.fa \
+        --sjdbGTFfile data/reference/GRCh38.gtf \
+        --sjdbOverhang 99
+fi
+index_dir_SIRV="./data/reference/STARindex_SIRV"
+mkdir -p $index_dir_SIRV
+if [ -z "$(ls -A ${index_dir_SIRV})" ]; then
+    STAR \
+        --runMode genomeGenerate \
+        --genomeDir ${index_dir_SIRV} \
+        --runThreadN 20 \
+        --genomeFastaFiles data/reference/SIRV_combined.fa \
+        --sjdbGTFfile data/reference/GRCh38_SIRV.gtf \
+        --sjdbOverhang 97
+fi
+mkdir -p ./data/03.Align
+getAlign() {
+    # if sampleID contains SIRV, align to SIRV reference
+    if [[ $1 == *"SIRV"* ]]; then
+        index_dir="./data/reference/STARindex_SIRV"
+    else
+        index_dir="./data/reference/STARindex"
+    fi
+    indir="data/02.Clean_data"
+    outdir="data/03.Align"
+    sampleID=$(basename $1 _1.fastq.gz)
+    # group=${line[2]}
+    # echo "Processing: "$sampleID::$group
+    STAR \
+        --runMode alignReads \
+        --twopassMode Basic \
+        --genomeDir ${index_dir} \
+        --runThreadN 10 \
+        --readFilesIn $1 ${1%_1.fastq.gz}_2.fastq.gz \
+        --readFilesCommand zcat \
+        --outFileNamePrefix $outdir/$sampleID \
+        --outSAMtype BAM SortedByCoordinate \
+        --outBAMsortingThreadN 5 \
+        --outFilterType BySJout \
+        --quantMode TranscriptomeSAM
+}
+export -f getAlign
+# date
+find ./data/02.Clean_data -name "*_1.fastq.gz" | parallel --progress --keep-order --line-buffer getAlign
+# date
+/data0/apps/anaconda3/bin/multiqc ./data/03.Align -o ./data/03.Align/report
