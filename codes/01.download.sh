a413863328503bb1da7f735176063706b414cd62
babaolanqiu <babaolanqiu@gmail.com>
2024-03-03 09:21:39 +0800

    Add fasta and gtf manipulation and annotation merging scripts

--- codes/01.download.sh
+++ codes/01.download.sh
@@ -13,3 +13,6 @@ awk -F'\t' 'NR>1 {print $1,$2"_"$3}' "data/01.Raw_data/samples.tsv" | while read
     mv "data/01.Raw_data/${old}_1.fastq.gz" "data/01.Raw_data/${new}_1.fastq.gz"
     mv "data/01.Raw_data/${old}_2.fastq.gz" "data/01.Raw_data/${new}_2.fastq.gz"
 done
+seqkit grep -r -p '^chr([0-9][0-9]?|X|Y)$' ./data/reference/GRCh38.fa >./data/reference/GRCh38_chr.fa
+seqkit concat ./data/reference/SIRV_Set1_Norm_Sequences_20210507/SIRV_isoforms_multi-fasta_170612a.fasta ./data/reference/GRCh38_chr.fa -o ./data/reference/SIRV_combined.fa -f
+agat_sp_merge_annotations.pl --gff ./data/reference/GRCh38_chr.gtf --gff data/reference/SIRV_Set1_Norm_Sequences_20210507/SIRV_isoforms_multi-fasta-annotation_C_170612a.gtf -o ./data/reference/GRCh38_SIRV.gtf
