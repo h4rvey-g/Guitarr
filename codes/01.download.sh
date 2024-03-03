68fc2e8e68295e15496f05f5be2a1d1ad28f7761
babaolanqiu <babaolanqiu@gmail.com>
2024-03-03 21:48:08 +0800

    Add samtools faidx commands and merge annotations

--- codes/01.download.sh
+++ codes/01.download.sh
@@ -15,4 +15,6 @@ awk -F'\t' 'NR>1 {print $1,$2"_"$3}' "data/01.Raw_data/samples.tsv" | while read
 done
 seqkit grep -r -p '^chr([0-9][0-9]?|X|Y)$' ./data/reference/GRCh38.fa >./data/reference/GRCh38_chr.fa
 seqkit concat ./data/reference/SIRV_Set1_Norm_Sequences_20210507/SIRV_isoforms_multi-fasta_170612a.fasta ./data/reference/GRCh38_chr.fa -o ./data/reference/SIRV_combined.fa -f
+samtools faidx ./data/reference/GRCh38_chr.fa
+samtools faidx ./data/reference/SIRV_combined.fa
 agat_sp_merge_annotations.pl --gff ./data/reference/GRCh38_chr.gtf --gff data/reference/SIRV_Set1_Norm_Sequences_20210507/SIRV_isoforms_multi-fasta-annotation_C_170612a.gtf -o ./data/reference/GRCh38_SIRV.gtf
