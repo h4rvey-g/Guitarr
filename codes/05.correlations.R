834f49b91a0e332f5a79a03d165537ea55465db1
babaolanqiu <babaolanqiu@gmail.com>
2024-05-09 07:29:40 +0000

    Replace %>% with |>

--- codes/05.correlations.R
+++ codes/05.correlations.R
@@ -11,26 +11,26 @@ combinations <- expand.grid(cells = cells, suffix = c("_1", "_2", "_3"), strings
 stringtie_tx_files <- list.files("data/04.Tx_quantification/01.Stringtie", pattern = "t_data.ctab", full.names = TRUE, recursive = TRUE)
 
 # use tximport to read all t_data.ctab files in stringtie_tx_files
-tx2gene <- read_tsv(stringtie_tx_files[1]) %>% select(t_name, gene_name)
+tx2gene <- read_tsv(stringtie_tx_files[1]) |> select(t_name, gene_name)
 stringtie_tx <- list(
     GS689 = tximport(stringtie_tx_files[grepl("GS689", stringtie_tx_files)], type = "stringtie", countsFromAbundance = "lengthScaledTPM", tx2gene = tx2gene, txOut = TRUE)$abundance,
     PC3E = tximport(stringtie_tx_files[grepl("PC3E", stringtie_tx_files)], type = "stringtie", countsFromAbundance = "lengthScaledTPM", tx2gene = tx2gene, txOut = TRUE)$abundance
-) %>%
-    map(~ as.data.frame(.x) %>%
-        rownames_to_column("Transcript_ID") %>%
+) |>
+    map(~ as.data.frame(.x) |>
+        rownames_to_column("Transcript_ID") |>
         as_tibble())
 
 #####################
 # 02. stringtie with imputation
 #####################
 stringtie_impute_files <- list.files("data/04.Tx_quantification/02.Stringtie_with_impute", pattern = "t_data.ctab", full.names = TRUE, recursive = TRUE)
-tx2gene <- read_tsv(stringtie_impute_files[1]) %>% select(t_name, gene_name)
+tx2gene <- read_tsv(stringtie_impute_files[1]) |> select(t_name, gene_name)
 stringtie_impute <- list(
     GS689 = tximport(stringtie_impute_files[grepl("GS689", stringtie_impute_files)], type = "stringtie", countsFromAbundance = "lengthScaledTPM", tx2gene = tx2gene, txOut = TRUE)$abundance,
     PC3E = tximport(stringtie_impute_files[grepl("PC3E", stringtie_impute_files)], type = "stringtie", countsFromAbundance = "lengthScaledTPM", tx2gene = tx2gene, txOut = TRUE)$abundance
-) %>%
-    map(~ as.data.frame(.x) %>%
-        rownames_to_column("Transcript_ID") %>%
+) |>
+    map(~ as.data.frame(.x) |>
+        rownames_to_column("Transcript_ID") |>
         as_tibble())
 #####################
 # 03. Guitar
@@ -57,63 +57,63 @@ guitar <- map(guitar, ~ select(.x, Transcript_ID, TPM))
 
 # bind columns of bam_impute respectively for GS689 and PC3E
 guitar_combined <- list()
-guitar_combined$GS689 <- inner_join(guitar$GS689_1, guitar$GS689_2, by = "Transcript_ID", suffix = c(".1", ".2")) %>%
+guitar_combined$GS689 <- inner_join(guitar$GS689_1, guitar$GS689_2, by = "Transcript_ID", suffix = c(".1", ".2")) |>
     inner_join(., guitar$GS689_3, by = "Transcript_ID", suffix = c("", ".3"))
-guitar_combined$PC3E <- inner_join(guitar$PC3E_1, guitar$PC3E_2, by = "Transcript_ID", suffix = c(".1", ".2")) %>%
+guitar_combined$PC3E <- inner_join(guitar$PC3E_1, guitar$PC3E_2, by = "Transcript_ID", suffix = c(".1", ".2")) |>
     inner_join(., guitar$PC3E_3, by = "Transcript_ID", suffix = c("", ".3"))
 
 #####################
 # 04. three_gen
 #####################
 # read tsv test/3rd_gen/3rd_gen.txt, and drop columns begin with HEK293T
-three_gen <- read_tsv("data/reference/3rd_gen/3rd_gen.txt") %>%
-    select(-starts_with("HEK293T")) %>%
+three_gen <- read_tsv("data/reference/3rd_gen/3rd_gen.txt") |>
+    select(-starts_with("HEK293T")) |>
     as_tibble()
-three_gen_gtf <- read.gtf("data/reference/3rd_gen/3rd_gen.gtf") %>%
-    as_tibble() %>%
+three_gen_gtf <- read.gtf("data/reference/3rd_gen/3rd_gen.gtf") |>
+    as_tibble() |>
     filter(feature == "transcript")
 
 # calculate length of each transcript
-three_gen_gtf <- three_gen_gtf %>%
-    mutate(transcript_length = three_gen_gtf$end - three_gen_gtf$start) %>%
+three_gen_gtf <- three_gen_gtf |>
+    mutate(transcript_length = three_gen_gtf$end - three_gen_gtf$start) |>
     select(transcript_id, transcript_length)
 
 # # remove _[digit] from Transcript_ID
-# three_gen$transcript_ID <- three_gen$transcript_ID %>% str_replace("_\\d+$", "")
+# three_gen$transcript_ID <- three_gen$transcript_ID |> str_replace("_\\d+$", "")
 three_gen_list <- list()
-three_gen_list$GS689 <- three_gen %>%
-    select(transcript_ID, starts_with("GS689")) %>%
-    dplyr::rename(long_read_1 = GS689_1, long_read_2 = GS689_2, long_read_3 = GS689_3) %>%
-    inner_join(., three_gen_gtf, by = c("transcript_ID" = "transcript_id")) %>%
+three_gen_list$GS689 <- three_gen |>
+    select(transcript_ID, starts_with("GS689")) |>
+    dplyr::rename(long_read_1 = GS689_1, long_read_2 = GS689_2, long_read_3 = GS689_3) |>
+    inner_join(., three_gen_gtf, by = c("transcript_ID" = "transcript_id")) |>
     mutate(transcript_ID = str_remove(transcript_ID, "_\\d+$"))
-three_gen_list$PC3E <- three_gen %>%
-    select(transcript_ID, starts_with("PC3E")) %>%
-    dplyr::rename(long_read_1 = PC3E_1, long_read_2 = PC3E_2, long_read_3 = PC3E_3) %>%
-    inner_join(., three_gen_gtf, by = c("transcript_ID" = "transcript_id")) %>%
+three_gen_list$PC3E <- three_gen |>
+    select(transcript_ID, starts_with("PC3E")) |>
+    dplyr::rename(long_read_1 = PC3E_1, long_read_2 = PC3E_2, long_read_3 = PC3E_3) |>
+    inner_join(., three_gen_gtf, by = c("transcript_ID" = "transcript_id")) |>
     mutate(transcript_ID = str_remove(transcript_ID, "_\\d+$"))
 
 # 计算 RPK
-three_gen_list$GS689 <- three_gen_list$GS689 %>%
+three_gen_list$GS689 <- three_gen_list$GS689 |>
     mutate(across(starts_with("long_read"), ~ . / transcript_length * 1000, .names = "RPK_{.col}"))
 
-three_gen_list$PC3E <- three_gen_list$PC3E %>%
+three_gen_list$PC3E <- three_gen_list$PC3E |>
     mutate(across(starts_with("long_read"), ~ . / transcript_length * 1000, .names = "RPK_{.col}"))
 
 # 计算总的 RPK
-total_RPK_GS689 <- select(three_gen_list$GS689, starts_with("RPK_")) %>%
-    sapply(sum) %>%
+total_RPK_GS689 <- select(three_gen_list$GS689, starts_with("RPK_")) |>
+    sapply(sum) |>
     sum()
-total_RPK_PC3E <- select(three_gen_list$PC3E, starts_with("RPK_")) %>%
-    sapply(sum) %>%
+total_RPK_PC3E <- select(three_gen_list$PC3E, starts_with("RPK_")) |>
+    sapply(sum) |>
     sum()
 
 # 计算 TPM
-three_gen_list$GS689 <- three_gen_list$GS689 %>%
-    mutate(across(starts_with("RPK_"), ~ (. / total_RPK_GS689) * 10^6, .names = "TPM_{str_remove(.col, 'RPK_')}")) %>%
+three_gen_list$GS689 <- three_gen_list$GS689 |>
+    mutate(across(starts_with("RPK_"), ~ (. / total_RPK_GS689) * 10^6, .names = "TPM_{str_remove(.col, 'RPK_')}")) |>
     select(transcript_ID, starts_with("TPM"))
 
-three_gen_list$PC3E <- three_gen_list$PC3E %>%
-    mutate(across(starts_with("RPK_"), ~ (. / total_RPK_PC3E) * 10^6, .names = "TPM_{str_remove(.col, 'RPK_')}")) %>%
+three_gen_list$PC3E <- three_gen_list$PC3E |>
+    mutate(across(starts_with("RPK_"), ~ (. / total_RPK_PC3E) * 10^6, .names = "TPM_{str_remove(.col, 'RPK_')}")) |>
     select(transcript_ID, starts_with("TPM"))
 #####################
 # 05. Calculate correlations
