b51d440da40ac311604c33a21e34f7093b06e5ac
babaolanqiu <babaolanqiu@gmail.com>
2024-05-27 08:53:20 +0000

    Add gene expression files to Guitar directory

--- codes/04_3.guitar.sh
+++ codes/04_3.guitar.sh
@@ -2,3 +2,4 @@
 mkdir -p ./data/04.Tx_quantification/03.Guitar
 # cp all files in data/04.Tx_quantification/02.Stringtie_with_impute/bam_impute/GS689_2Aligned.sortedByCoord.out/ end with transcript.expression.txt
 cp ./data/04.Tx_quantification/02.Stringtie_with_impute/bam_impute/*/*transcript.expression.txt ./data/04.Tx_quantification/03.Guitar/
+cp ./data/04.Tx_quantification/02.Stringtie_with_impute/bam_impute/*/*gene.expression.txt ./data/04.Tx_quantification/03.Guitar/
